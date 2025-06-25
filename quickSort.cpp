c66e3057794e8e99ebde23e75c55f49f708964ef
Motorrr <m2400@bk.ru>
2025-06-26 01:22:30 +0300

    Add files via upload
    Реализована логика сохранения файлов

--- quickSort.cpp
+++ quickSort.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>   
 #include <time.h>    
+#include <string.h>
 
 void swap(int* a, int* b) {
     int temp = *a;
@@ -38,16 +39,32 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 
+void ensure_txt_extension(char* filename) {
+    if (strstr(filename, ".txt") == NULL) {
+        strcat(filename, ".txt");
+    }
+}
+
 int main() {
     const int n = 20;
     int arr[n];
     int choice;
-    int min, max;       
-    int temp;          
+    int min, max;
+    int temp;
+    char input_filename[100];  
+    char output_filename[100]; 
 
     srand(time(NULL));
 
-    printf("Выберите способ заполнения массива:\n");
+    printf("Введите название файла для исходных данных: ");
+    scanf("%99s", input_filename);
+    ensure_txt_extension(input_filename);
+
+    printf("Введите название файла для результатов сортировки: ");
+    scanf("%99s", output_filename);
+    ensure_txt_extension(output_filename);
+
+    printf("\nВыберите способ заполнения массива:\n");
     printf("1 - Ввести 20 чисел вручную\n");
     printf("2 - Автоматическое заполнение случайными числами\n");
     printf("Ваш выбор: ");
@@ -85,7 +102,36 @@ int main() {
         printf("Ошибка! Неверный выбор.\n");
         return 1;
     }
+
+    FILE* input_file = fopen(input_filename, "w");
+    if (input_file == NULL) {
+        printf("Ошибка создания файла для исходных данных!\n");
+        return 1;
+    }
+
+    fprintf(input_file, "Исходные данные:\n");
+    for (int i = 0; i < n; i++) {
+        fprintf(input_file, "%d ", arr[i]);
+    }
+    fprintf(input_file, "\n");
+    fclose(input_file);
+    printf("\nИсходные данные сохранены в файл: %s\n", input_filename);
+
     quickSort(arr, 0, n - 1);
+
+    FILE* output_file = fopen(output_filename, "w");
+    if (output_file == NULL) {
+        printf("Ошибка создания файла для результатов!\n");
+        return 1;
+    }
+
+    fprintf(output_file, "Отсортированный массив:\n");
+    for (int i = 0; i < n; i++) {
+        fprintf(output_file, "%d ", arr[i]);
+    }
+    fprintf(output_file, "\n");
+    fclose(output_file);
+    printf("Результаты сортировки сохранены в файл: %s\n", output_filename);
 
     printf("\nОтсортированный массив:\n");
     for (int i = 0; i < n; i++) {
